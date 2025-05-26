3333005701f81a566365052be663fd2409872515
MyLuvlyP <a01085838583@gmail.com>
2025-05-26 10:50:06 +0900

    Add files via upload
    first file

---
+++ web/include/js/url.js
@@ -0,0 +1,16 @@
+function one(){
+    location.href="http://127.0.0.1:5500/sub/one.html"
+}
+function two(){
+    location.href="http://127.0.0.1:5500/sub/two.html"
+}
+
+function three(){
+    location.href="http://127.0.0.1:5500/sub/three.html"
+}
+function four(){
+    location.href="http://127.0.0.1:5500/sub/four.html"
+}
+function five(){
+    location.href="http://127.0.0.1:5500/sub/five.html"
+}
